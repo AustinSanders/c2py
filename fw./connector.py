3039483c6e8f65e94453f64bd757e067e030e086
AustinSanders <as2544@nau.edu>
2017-05-31 17:20:07 -0700

    Cleaned fw / fw.

--- fw./connector.py
+++
@@ -1,8 +0,0 @@
-# connector.py
-# Special purpose component for managing communication
-import fw
-
-class Connector(fw.Component):
-
-    def __init__(self, id, behavior=None):
-        super().__init__(id, behavior)
