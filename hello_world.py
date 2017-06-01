d39e58d7b67de91b8a03846e2d3ae1ef0b7609ca
AustinSanders <as2544@nau.edu>
2017-05-31 17:25:16 -0700

    changed hello_world.py to message sending example.

--- hello_world.py
+++ hello_world.py
@@ -1,123 +1,108 @@
+import sys
+sys.path.append(r'C:\Users\as2544\C2Py')
 import fw
-import time
 from queue import Empty
+import time
 
+# this is a one-way conversation from top (sender) to bottom (receiver).
+
+# This is important for router to connect things.  This creates a listener on an interface to a
+# dispatcher.  The listener is put into the list of the interface (list of listeners).
+
+# flow = from interface to listener to disptacher
 def connect(interface_pair, dispatcher_pair, id):
     el = fw.EventListener(id, dispatcher_pair[0].get_event_dispatcher(dispatcher_pair[1]))
     interface_pair[0].get_interface(interface_pair[1]).add_event_listener(el)
 
+
+# so need handler and behavior.  FLow = compentent to dispatcher.  THis is the TOP component.
+
 class Sender(fw.Component):
+    def sender_behavior(self):        
+       message = input("Give a message to send: ")
+       e = fw.Event({'source':str(self.id),'message':str(message)})
+       self.fire_event_on_interface(e, "bottom")
+       # don't need dispatcher on loop
 
-    class NotificationHandler():
-        def handle(self, event):
-            print("{0} received a notification:\n{1}\n".format(event.context["owner"], event))
-
-    def sender_behavior(self):
-        notifications_received = 0
-        while notifications_received < 32:
-            for dispatcher in self.event_dispatchers:
-                try:
-                    dispatcher.dispatch_event()
-                    notifications_received += 1
-                except Empty:
-                    pass
-
-            if notifications_received % 2 == 0:
-                e = fw.Event("Message: {0}".format(notifications_received / 2))
-                self.fire_event_on_interface(e, "top")
-                time.sleep(.5)
-
+    # don't need to have top interface because this is the top.  Also, don't need dispatcher because expecting
+    # no events from bottom.
     def __init__(self, id):
         super().__init__(id, self.sender_behavior)
-        self.add_interface(fw.EventInterface("top"))
-        from_top = fw.EventDispatcher("notification_dispatcher", self, 0.1)
-        from_top.add_event_handler(self.NotificationHandler())
-        self.add_event_dispatcher(from_top)
+        self.add_interface(fw.EventInterface("bottom"))
         self.properties["owner"] = self
 
 
-class Receiver(fw.Component):
+# THIS is the bottom component. and we are just listening for events to come in.
 
+# will need a handler
+class Receiver(fw.Component):
+    # IMportant to have.  Top may not get reply in one way exchange
     class RequestHandler():
         def handle(self, event):
-            print("{0} received a request:\n{1}\n".format(event.context["owner"], event))
-            event.context["owner"].fire_event_on_interface(event, "bottom")
+            print("{0} received a notification:\n{1}\n".format(event.payload()['source'], event.payload()['message']))
 
     def receiver_behavior(self):
-        received_messages = 0
-        while received_messages < 16:
-            for dispatcher in self.event_dispatchers:
-                try:
-                    dispatcher.dispatch_event()
-                    received_messages += 1
-                except Empty:
-                    pass
+        # this will take the event and queus and send to handler.
+        for dispatcher in self.event_dispatchers:
+            try:
+                dispatcher.dispatch_event()
+                # TODO: possible break command. (component.stop)
+            except Empty:
+                pass
 
     def __init__(self, id):
         super().__init__(id, self.receiver_behavior)
-        self.add_interface(fw.EventInterface("bottom"))
-        from_bot = fw.EventDispatcher("request_dispatcher", self, 0.1)
-        from_bot.add_event_handler(self.RequestHandler())
-        self.add_event_dispatcher(from_bot)
+        from_top = fw.EventDispatcher("request_dispatcher", self, 0.1)
+        from_top.add_event_handler(self.RequestHandler())
+        self.add_event_dispatcher(from_top)
         self.properties["owner"] = self
 
 
+
+
+# will need this for connector. no need for special behavhoir.
 class Router(fw.Connector):
 
-    class NotificationHandler():
-        def handle(self, event):
-            event.context["owner"].fire_event_on_interface(event, "bottom")
+    # No need for notification handler because of top down communication.
+    # class NotificationHandler():
+    #     def handle(self, event):
+    #         event.context["owner"].fire_event_on_interface(event, "bottom")
 
     class RequestHandler():
         def handle(self, event):
-            event.context["owner"].fire_event_on_interface(event, "top")
+            # Shoot the message to the bottom interface of the router.
+            event.context()['owner'].fire_event_on_interface(event, "bottom")
 
     def router_behavior(self):
-        forwarded_notifications = 0
-        while forwarded_notifications < 32:
-            for dispatcher in self.event_dispatchers:
-                try:
-                    dispatcher.dispatch_event()
-                    if dispatcher.id == "notification_dispatcher":
-                        forwarded_notifications += 1
-                except Empty:
-                    pass
+        for dispatcher in self.event_dispatchers:
+            try:
+                dispatcher.dispatch_event()
+            except Empty:
+                pass
 
     def __init__(self, id):
         super().__init__(id, self.router_behavior)
-        self.add_interface(fw.EventInterface("top"))
         self.add_interface(fw.EventInterface("bottom"))
-        from_top = fw.EventDispatcher("notification_dispatcher", self, 0.1)
-        from_top.add_event_handler(self.NotificationHandler())
+        from_top = fw.EventDispatcher("request_dispatcher", self, 0.1)
+        # add a handler to the dispatcher
+        from_top.add_event_handler(self.RequestHandler())
         self.add_event_dispatcher(from_top)
-        from_bot = fw.EventDispatcher("request_dispatcher", self, 0.1)
-        from_bot.add_event_handler(self.RequestHandler())
-        self.add_event_dispatcher(from_bot)
         self.properties["owner"] = self
 
-
-
+# Idea is to connect bottom interface of top component to top interface of connector/router, connect bottom interface
+# of router to request dispatcher of bottom component.
 if __name__ == "__main__":
     sender = Sender("Sender")
     router = Router("Router")
-    receiver_a = Receiver("Receiver_A")
-    receiver_b = Receiver("Receiver_B")
+    receiver = Receiver("Receiver")
 
-    connect((sender, "top"), (router, "request_dispatcher"), 0)
+    connect((sender, "bottom"), (router, "request_dispatcher"), 0)
 
-    connect((router, "bottom"), (sender,     "notification_dispatcher"), 0)
-    connect((router, "top"),    (receiver_a, "request_dispatcher"), 0)
-    connect((router, "top"),    (receiver_b, "request_dispatcher"), 1)
+    connect((router, "bottom"), (receiver,     "request_dispatcher"), 0)
 
-    connect((receiver_a, "bottom"), (router, "notification_dispatcher"), 0)
-    connect((receiver_b, "bottom"), (router, "notification_dispatcher"), 0)
 
-    receiver_a.start()
-    receiver_b.start()
-    router.start()
     sender.start()
+    receiver.start()
+    router.start()
 
-    receiver_a.join()
-    receiver_b.join()
-    router.join()
     sender.join()
